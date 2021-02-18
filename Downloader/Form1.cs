1b184f1e841019cb4fb9d6656b7f96f42d8c871f
bfresnel <benoit.fresnel.1@gmail.com>
2021-02-18 01:41:21 +0100

    AdoptOpenJDK download

--- Downloader/Form1.cs
+++ Downloader/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.ComponentModel;
 
 namespace Downloader
 {
@@ -15,6 +10,28 @@ namespace Downloader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void wc_DownloadProgressChanged(object s, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        void wc_DownloadFileCompleted(object s, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Téléchargement terminé", "info",  MessageBoxButtons.OK);
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Démarrage du téléchargement du JDK");
+            WebClient wc = new WebClient();
+            wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(wc_DownloadProgressChanged);
+            wc.DownloadFileCompleted += new AsyncCompletedEventHandler(wc_DownloadFileCompleted);
+            wc.DownloadFileAsync(new Uri("https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.10%2B9/OpenJDK11U-jdk_x64_windows_hotspot_11.0.10_9.msi"),
+                "OpenJDK11U-jdk_x64_windows_hotspot_11.0.10_9.msi");
+            button1.Enabled = false;
         }
     }
 }
